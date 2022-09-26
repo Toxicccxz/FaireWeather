23817b47e95f405adec4e976401aa870b9ee0dc7
Xavier Cui <toxicstrikercuixinzhou@gmail.com>
2022-09-25 23:43:01 -0400

    Update SharedUtils.java

--- app/src/main/java/com/fiare/fiareweather/utils/SharedUtils.java
+++ app/src/main/java/com/fiare/fiareweather/utils/SharedUtils.java
@@ -9,7 +9,7 @@ public class SharedUtils {
     public static SharedPreferences.Editor editor;
 
     public static void putKey(Context context, String Key, String Value) {
-        sharedPreferences = context.getSharedPreferences("prodapt", Context.MODE_PRIVATE);
+        sharedPreferences = context.getSharedPreferences("faire", Context.MODE_PRIVATE);
         editor = sharedPreferences.edit();
         editor.putString(Key, Value);
         editor.apply();
@@ -17,17 +17,17 @@ public class SharedUtils {
     }
 
     public static String getKey(Context contextGetKey, String Key) {
-        sharedPreferences = contextGetKey.getSharedPreferences("prodapt", Context.MODE_PRIVATE);
+        sharedPreferences = contextGetKey.getSharedPreferences("faire", Context.MODE_PRIVATE);
         return sharedPreferences.getString(Key, "");
     }
 
     public static String getKey(Context contextGetKey, String Key, String defaultValue) {
-        sharedPreferences = contextGetKey.getSharedPreferences("prodapt", Context.MODE_PRIVATE);
+        sharedPreferences = contextGetKey.getSharedPreferences("faire", Context.MODE_PRIVATE);
         return sharedPreferences.getString(Key, defaultValue);
     }
 
     public static void clearSharedPreferences(Context context) {
-        sharedPreferences = context.getSharedPreferences("prodapt", Context.MODE_PRIVATE);
+        sharedPreferences = context.getSharedPreferences("faire", Context.MODE_PRIVATE);
         sharedPreferences.edit().clear().apply();
     }
 }
